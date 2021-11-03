bd0ee29347f17f573f86bea1c09f643fa4051a1b
NinjagoRT <34011364+NinjagoART@users.noreply.github.com>
2021-11-03 10:03:08 -0600

    Add files via upload

---
+++ cal_Rndm.c
@@ -0,0 +1,4 @@
+	//srand(time(NULL)); //Genera una Semilla a partir del tiempo
+	
+	//	rnbr  = rand() % 101; //genra valores desde 0 a un limite de 100
+	//	rnbr  = rand() % (100-70+1)+70; //genra valores desde 70 a 100
